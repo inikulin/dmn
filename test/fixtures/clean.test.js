5116570280c16bc040fdae9184c127192d850b3c
inikulin <ifaaan@gmail.com>
2014-09-12 01:21:29 +0400

    =Clean tests

--- test/fixtures/clean.test.js
+++ test/fixtures/clean.test.js
@@ -6,9 +6,13 @@ var fs = require('fs-extra'),
 
 cli.silent = true;
 
-describe('clean', function () {
-    this.timeout(99999);
+//NOTE: ensureDir which can be safely passed to Array.forEach()
+//(learn more: http://www.wirfs-brock.com/allen/posts/166)
+function ensureDirSync(dir) {
+    fs.ensureDirSync(dir, 0777);
+}
 
+describe('clean', function () {
     var tmpPath = path.join(__dirname, '../tmp');
 
     beforeEach(function () {
@@ -49,11 +53,9 @@ describe('clean', function () {
             ];
 
         filesToClean.concat(filesToIgnore).forEach(fs.ensureFileSync);
-        dirsToClean.concat(dirsToIgnore).forEach(function (dir) {
-            fs.ensureDirSync(dir);
-        });
+        dirsToClean.concat(dirsToIgnore).forEach(ensureDirSync);
 
-        dmn.clean(tmpPath, {silent: true, force: true}).then(function (status) {
+        dmn.clean(tmpPath, {force: true}).done(function (status) {
             status.should.eql('cleaned');
 
             filesToClean.concat(dirsToClean).forEach(function (file) {
@@ -61,6 +63,100 @@ describe('clean', function () {
             });
 
             filesToIgnore.concat(dirsToIgnore).forEach(function (file) {
+                fs.existsSync(file).should.be.true;
+            });
+
+            done();
+        });
+    });
+
+    it('should do nothing if there is no node_modules directory in the project', function (done) {
+        var projectFiles = [
+                'index.js',
+                'Readme.md',
+                'package.json'
+            ],
+            projectDirs = [
+                'lib',
+                'src'
+            ];
+
+        projectFiles.forEach(fs.ensureFileSync);
+        projectDirs.forEach(ensureDirSync);
+
+        dmn.clean(tmpPath, {force: true}).done(function (status) {
+            status.should.eql('no-deps');
+
+            projectFiles.concat(projectDirs).forEach(function (file) {
+                fs.existsSync(file).should.be.true;
+            });
+
+            done();
+        });
+    });
+
+    it('should do nothing if dependencies are already clean', function (done) {
+        var projectFiles = [
+                '.npmignore',
+                'node_modules/yo/index.js',
+                'node_modules/yo/node_modules/yoyo/package.json',
+                'node_modules/awesome_package/Readme.md'
+            ],
+            projectDirs = [
+                'node_modules/yo/lib',
+                'node_modules/yo/node_modules/yoyo/src',
+                'lib',
+                'test'
+            ];
+
+        projectFiles.forEach(fs.ensureFileSync);
+        projectDirs.forEach(ensureDirSync);
+
+        dmn.clean(tmpPath, {force: true}).done(function (status) {
+            status.should.eql('already-perfect');
+
+            projectFiles.concat(projectDirs).forEach(function (file) {
+                fs.existsSync(file).should.be.true;
+            });
+
+            done();
+        });
+    });
+
+    it('should cancel cleaning on user demand if "force" flag disabled', function (done) {
+        var projectFiles = [
+                '.npmignore',
+                'node_modules/yo/.travis.yml',
+                'node_modules/awesome_package/.gitignore',
+                'node_modules/awesome_package/Gruntfile.js',
+                'node_modules/yo/node_modules/yoyo/Makefile',
+                'node_modules/yo/node_modules/yoyo/Changes',
+                'node_modules/yo/index.js',
+                'node_modules/yo/node_modules/yoyo/package.json',
+                'node_modules/awesome_package/Readme.md'
+            ],
+            projectDirs = [
+                'node_modules/yo/benchmark',
+                'node_modules/awesome_package/examples',
+                'node_modules/yo/node_modules/yoyo/test',
+                'node_modules/yo/node_modules/yoyo/.coverage_data/',
+                'node_modules/yo/lib',
+                'node_modules/yo/node_modules/yoyo/src',
+                'lib',
+                'test'
+            ];
+
+        projectFiles.forEach(fs.ensureFileSync);
+        projectDirs.forEach(ensureDirSync);
+
+        cli.confirm = function (what, callback) {
+            callback(false);
+        };
+
+        dmn.clean(tmpPath, {force: false}).done(function (status) {
+            status.should.eql('canceled');
+
+            projectFiles.concat(projectDirs).forEach(function (file) {
                 fs.existsSync(file).should.be.true;
             });
 
